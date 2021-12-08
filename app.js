df79c98f2556be3a752ae9cf9969672a54957d63
ddmorrison <danidelemorr26@outlook.com>
2021-12-08 09:54:29 -0600

    JavaVariables

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+//Exercise 1
+const firstName = "Danielle";
+let lastName = "Morrison";
+var age = 31;
+
+//Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+//Bonus
+templateFullName = `${firstName} ${lastName}`;
+console.log(templateFullName);
+
+//Exercise 3
+let city= "Killen"
+let passTime= "cooking"
+let myStory=`Hi! My name is ${fullName}. I live in ${city}. I enjoy ${passTime}and coding.`
+console.log(myStory);
